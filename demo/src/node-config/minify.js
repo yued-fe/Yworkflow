f61ecf24a87d6e5447004424f182ef6f342627b0
p_leilluo(罗磊) <luolei@yuewen.com>
2016-09-30 11:17:25 +0800

    修正文档

---
+++ demo/src/node-config/minify.js
@@ -0,0 +1,15 @@
+/**
+ * 服务器render压缩html配置
+ *
+ */
+var minifyConfig =
+    {
+        "removeAttributeQuotes": "false",
+        "removeComments": "true",
+        "collapseWhitespace": "true",
+        "collapseInlineTagWhitespace":"true",
+        "minifyCSS": "false",
+        "minifyJS": "false"
+    }
+
+module.exports = minifyConfig;
