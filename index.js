8f3b3d603b02d1bef75ebcd2f59ba422dde11d3b
luolei <i@luolei.org>
2017-06-28 14:58:14 +0800

    增加yworkcli兼容

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const { execSync } = require('child_process');
 
 exports = module.exports = ({path}) => {
     if (!path) {
-        console.error('argv path must be set.');
-        throw new Error('argv path must be set.');
+        console.error('无指定--yconfig配置,默认寻找当前目录下的.yconfig文件');
+        throw new Error('请指定--yconfig配置');
         return;
     }
-
     return execSync('cd ' + __dirname + ' && gulp build --path ' + path);
 };
