5d858b08d07cf5d1e682e0af67eb7dad956b4d3c
liupeng <liupeng0827@f-road.com.cn>
2017-11-03 15:55:39 +0800

    fix inline

--- tasks/inline.js
+++ tasks/inline.js
@@ -15,29 +15,40 @@ var TASK_CONFIG = PROJECT_CONFIG.tasks.inline;
 var path = require('path');
 
 // prereplace task
-var replaceConfig = path.join(PROJECT_ABS_PATH, TASK_CONFIG.replace);
-var baseDir = TASK_CONFIG.baseDir;
-var htmlPath = path.resolve(PROJECT_ABS_PATH, baseDir, TASK_CONFIG.htmlPath);
-var htmlDest = path.resolve(PROJECT_ABS_PATH, baseDir, TASK_CONFIG.htmlDest);
-
-
+var replaceConfig = TASK_CONFIG.replace;
+var baseDir = path.resolve(PROJECT_ABS_PATH, TASK_CONFIG.baseDir);
+var htmlPath = path.resolve(baseDir, TASK_CONFIG.htmlPath);
+var htmlDest = path.resolve(baseDir, TASK_CONFIG.htmlDest);
 
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 var usemin = require('gulp-usemin');
 var cleanCss = require('gulp-clean-css');
-var eof = require('end-of-stream');
-var path = require('path');
+var eos = require('end-of-stream');
 var chalk = require('chalk');
 var runSequence = require('run-sequence');
 
+// 因为inline之前基本都需要一个替换的动作,所以inline前先执行一个replace的操作
 gulp.task('inline:replace', function (cb) {
     
     function replaceTask(replaceItem, cb) {
-        return function () { eof(
-            gulp.src(path.resolve(PROJECT_ABS_PATH, baseDir, replaceItem.src))
-                .pipe(replace(replaceItem.replaceFrom, replaceItem.replaceTo))
-                .pipe(gulp.dest(path.resolve(PROJECT_ABS_PATH, baseDir, replaceItem.dest))), function () {
+        var srcPath = path.resolve(baseDir, replaceItem.src);
+        var destPath = path.resolve(baseDir, replaceItem.dest);
+        var patterns = replaceItem.patterns;
+        var patternKeys = Object.keys(patterns);
+        function patternTask(task, from, to) {
+           return task.pipe(replace(from, to));
+        }
+        return function () { eos(
+            (function () {
+                var task = gulp.src(srcPath);
+                patternKeys.forEach(function (key) {
+                    task = patternTask(task, key, patterns[key]); 
+                });
+                task.pipe(gulp.dest(destPath));
+                return task;
+            })(), function (err) {
+                if(err) return console.error(err);
                 cb();
             }
         )}
@@ -57,17 +68,22 @@ gulp.task('inline:usemin', function () {
     
     return gulp.src(htmlPath)
         .pipe(usemin({
-          inlinecss: [ cleanCss(), 'concat' ]
+          inlinecss: [ cleanCss, 'concat' ],
+          path: baseDir
         }))
         .pipe(gulp.dest(htmlDest));
 });
 
 
 gulp.task('inline', function(done) {
-    runSequence('inline:replace', 'inline:usemin',  done);
+    // 如果有replace则执行replace
+    if(replaceConfig.replace) {
+        runSequence('inline:replace', 'inline:usemin', done);
+    } else {
+        runSequence('inline:usemin', done);
+    }
 });
 
-
 gulp.task('inline:build', function(done) {
-    runSequence('inline:replace', 'inline:usemin',  done);
+    runSequence('inline', done);
 });
